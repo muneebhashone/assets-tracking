5e3159f79fd1bc069cddb713c5604d5fa9da6207
InaaraKalani <kalani.inaara@gmail.com>
2025-04-15 12:11:35 +0500

    Header linking fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,30 +2,22 @@
 
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { menuitems } from "../constants/data";
 import LoginButton from "./LoginButton";
 import Menu from "./Menu";
 import NavLogo from "./NavLogo";
 import { UserNav } from "./layout/user-nav";
-
-
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
-
   const [open, setOpen] = useState(false);
-  const router = useRouter();
-  const handleOpen = () => {
-    setOpen(!open);
-  };
   const { status } = useSession();
 
   return (
     <>
-
-      <div
-        className="flex-row md:flex items-center justify-between bg-[#FFFFFF] fixed w-full px-5 md:px-20 lg:px-60 z-[9999] shadow-lg">
+      <div className="flex-row md:flex items-center justify-between bg-[#FFFFFF] fixed w-full px-5 md:px-20 lg:px-60 z-[9999] shadow-lg">
         <div className="flex items-center justify-between">
           <NavLogo
             height={60}
@@ -34,39 +26,48 @@ const Header = () => {
             width={60}
             alt="logo"
           />
-          <button onClick={handleOpen}>
+          <button onClick={() => setOpen(!open)}>
             <HamburgerMenuIcon className="md:hidden w-8 h-8" />
           </button>
         </div>
         <div className="flex-row md:flex items-center gap-28">
-          <Menu className={`${open ? "block" : "hidden"}`} listitem={menuitems} />
+          <Menu
+            className={`${open ? "block" : "hidden"}`}
+            listitem={menuitems}
+          />
           <div className="flex items-center gap-10 justify-center">
-
-            {status === "authenticated" ? <>
-                <UserNav />
-              </> :
+            {status === "authenticated" ? (
               <>
-                <LoginButton
-                  onclick={() => router.push("/signin")}
-                  title="Login"
-                  classname={`bg-transparent text-[#3491FE] font-medium md:block ${open ? "block" : "hidden"
-                  }`}
-                />
-                <LoginButton
-                  onclick={() => router.push("/signup")}
-                  title="Sign Up"
-                  classname={`bg-[#3491FE] text-white font-medium md:block ${open ? "block" : "hidden"
-                  }`}
-                />
+                <UserNav />
               </>
-            }
+            ) : (
+              <>
+                <Link href="/signin" passHref>
+                  <LoginButton
+                    title="Login"
+                    classname={cn(
+                      "bg-transparent text-[#3491FE] font-medium md:block",
+                      open ? "block" : "hidden",
+                    )}
+                  />
+                </Link>
+                <Link href="/signup" passHref>
+                  <LoginButton
+                    title="Sign Up"
+                    classname={cn(
+                      "bg-[#3491FE] text-white font-medium md:block",
+                      open ? "block" : "hidden",
+                    )}
+                  />
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
       ;
     </>
-  )
-    ;
+  );
 };
 
 export default Header;
